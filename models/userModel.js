1ec9a7d8509688a4dd2fbbd877c437f2e59297d5
ducminh1902 <leducminh22003@gmail.com>
2026-03-17 13:32:51 +0700

    first commit

--- models/userModel.js
+++ models/userModel.js
@@ -7,17 +7,10 @@ const userSchema = new mongoose.Schema({
 });
 
 // hash password before saving
-userSchema.pre('save', function(next) {
-  if (!this.isModified('password')) return next();
-  const user = this;
-  bcrypt.genSalt(10, function(err, salt) {
-    if (err) return next(err);
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
-      user.password = hash;
-      next();
-    });
-  });
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // helper to compare cleartext password with hashed one
